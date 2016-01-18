0103cc5941fa24ba8b02d4b652f482bed4d00674
Stargrazer82301 <Stargrazer82301@users.noreply.github.com>
2016-01-18 18:12:40 +0000

    Initial commit

---
+++ CAAPR.py
@@ -0,0 +1,15 @@
+# Import smorgasbord
+print 'Importing smorgasbord...'
+import os
+import sys
+sys.path.insert(0, '../')
+import gc
+import pdb
+import numpy as np
+import matplotlib.pyplot as plt
+import astropy
+import astropy.io.fits
+import astropy.io.votable
+import astropy.wcs
+import ChrisFuncs
+plt.ioff()
---
+++ __init__.py
@@ -0,0 +1,4 @@
+from . import CAAPR
+from .CAAPR import *
+#from . import Coadd
+from .. import ChrisFuncs
