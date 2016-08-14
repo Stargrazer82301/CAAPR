2fb8b3435d26e9ec3192239209641a3ddeb2ca70
Stargrazer82301 <Stargrazer82301@users.noreply.github.com>
2016-08-14 13:52:04 +0100

    Undid the prior attempt to fix thumb grid black borders, tying new way

--- CAAPR_Aperture/CAAPR_Aperture.py
+++ CAAPR_Aperture/CAAPR_Aperture.py
@@ -513,7 +513,7 @@ def ExcludedSubpipelineAperture(aperture_combined, source_dict, band_dict, kwarg
     img_naxis_pix = np.max([ pod['in_header']['NAXIS1'], pod['in_header']['NAXIS1'] ])
     img_naxis_arcsec = float(img_naxis_pix) * float(pod['pix_arcsec'])
     img_rad_arcsec = img_naxis_arcsec / 2.0
-    thumb_rad_arcsec = 1.1 * aperture_combined[0] * band_dict['annulus_outer']
+    thumb_rad_arcsec = 1.25 * aperture_combined[0] * band_dict['annulus_outer']
     CAAPR.CAAPR_IO.ThumbCutout(source_dict, band_dict, kwargs_dict, pod['in_fitspath'], img_rad_arcsec, thumb_rad_arcsec)
 
     # Rename thumbnail cutout, and make it the 'active' map by repeating necessary processing
