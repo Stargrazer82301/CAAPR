7e317acb9a790f59da091c012acce0af7cbd786f
Wouter Dobbels <wouter.dobbels@ugent.be>
2020-05-20 17:39:32 +0200

    Run from json config file

---
+++ run.py
@@ -0,0 +1,20 @@
+import os
+import sys
+import json
+import CAAPR
+
+if len(sys.argv) <= 1:  # No config file given as argument: use default
+    config_filename = './caapr_config.json'
+else:
+    config_filename = sys.argv[1]
+if not os.path.exists(config_filename):
+    raise ValueError("Can not find config file at " + config_filename)
+
+with open(config_filename, 'r') as config_file:
+    d_config = json.load(config_file)
+
+print('Starting CAAPR with following config: ' + str(d_config))
+CAAPR.CAAPR_Main.Run(**d_config)
+
+# Jubilate
+print('All done!')
